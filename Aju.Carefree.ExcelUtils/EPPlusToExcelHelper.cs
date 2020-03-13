b26df8cc6196228f0a384f22074ead17347e4780
AjuPrince <1022560838@qq.com>
2020-03-14 00:43:32 +0800

    添加项目文件。

---
+++ Aju.Carefree.ExcelUtils/EPPlusToExcelHelper.cs
@@ -0,0 +1,104 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Aju.Carefree.ExcelUtils
+{
+    /// <summary>
+    /// EPPlusToExcel 
+    /// <para>EPPlus 导出 EXCEL 帮助类</para>
+    /// </summary>
+    public static class EPPlusToExcelHelper
+    {
+        /// <summary>
+        /// 创建Excel 并保存 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="filePath">excel保持路径</param>
+        /// <param name="sheetName">sheet 名称</param>
+        /// <param name="listData">数据</param>
+        /// <param name="func">表头自定义，如果为空，则自动生成表头，复杂表头请自定义
+        /// <para>int：表格内容从第几行开始</para>  
+        /// </param>
+        public static void CreateExcelByList<T>(string filePath, string sheetName, List<T> listData,
+            Func<ExcelWorksheet, (ExcelWorksheet, int)> func = null) where T : class
+        {
+            Type t = typeof(T);
+            if (string.IsNullOrEmpty(filePath)) throw new NullReferenceException($"参数{nameof(filePath)}不能为空.");
+            if (string.IsNullOrEmpty(sheetName)) sheetName = "sheet1";
+            if (listData == null || listData.Count == 0) throw new NullReferenceException("导出内容不能为空.");
+            if (File.Exists(filePath))
+                File.Delete(filePath);
+            FileInfo newfile = new FileInfo(filePath);
+            using (ExcelPackage package = new ExcelPackage(newfile))
+            {
+                //在工作簿中获得第一个工作表
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(sheetName);
+                //表头
+                int row = 1;
+                if (func == null)
+                {
+                    t.GetProperties().ToList().ForEach((n) =>
+                    {
+                        if (!n.IsDefined(typeof(ExcelColumnAttribute), true))
+                            return;
+                        ExcelColumnAttribute attribute = (ExcelColumnAttribute)n.GetCustomAttribute(typeof(ExcelColumnAttribute), true);
+                        if (attribute != null)
+                            worksheet.Cells[row, attribute.Column].Value = attribute.Name;
+                    });
+                }
+                else
+                    (worksheet, row) = func.Invoke(worksheet);
+                foreach (var item in listData)
+                {
+                    int j = 0;
+                    foreach (var p in t.GetProperties())
+                    {
+                        if (p.IsDefined(typeof(ExcelColumnAttribute), true))
+                        {
+                            ExcelColumnAttribute attribute = (ExcelColumnAttribute)p.GetCustomAttribute(typeof(ExcelColumnAttribute), true);
+                            if (attribute != null)
+                            {
+                                var value = p.GetValue(item);
+                                worksheet.Cells[row + 1, attribute.Column].Value = p.GetValue(item).ToString();
+                                j++;
+                            }
+                        }
+                    }
+                    row++;
+                }
+                package.Save();
+            }
+
+        }
+    }
+
+    /// <summary>
+    /// Excel 列
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class ExcelColumnAttribute : Attribute
+    {
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        ///  Excel 列数
+        /// </summary>
+        public int Column { get; set; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="column">列数 第几列 </param>
+        public ExcelColumnAttribute(int column, string name)
+        {
+            Column = column;
+            Name = name;
+        }
+    }
+}
---
+++ Aju.Carefree/Program.cs
@@ -0,0 +1,51 @@
+﻿using Aju.Carefree.ExcelUtils;
+using System;
+using System.Collections.Generic;
+
+namespace Aju.Carefree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Person>();
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(new Person
+                {
+                    Sex = "Sex" + i,
+                    Age = "Age" + i,
+                    Brithday = DateTime.Now,
+                    Name = "Name" + i
+                });
+            }
+            EPPlusToExcelHelper.CreateExcelByList(@"D:\123.xlsx", "", list, ws =>
+            {
+                //ws.Cells[1, 1, 1, 2].Merge = true;
+                //ws.Cells[1, 1, 1, 2].Merge = true;
+                //ws.Cells[1, 1].Value = "姓名";
+
+                //ws.Cells[1, 2].Value = "年龄";
+
+                //ws.Cells[1, 3, 1, 4].Merge = true;
+                //ws.Cells[1, 3].Value = "生日";
+
+                //ws.Cells[1, 4].Value = "性别";
+                //return (ws, 1);
+            });
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Person
+    {
+        [ExcelColumn(1, "姓名")]
+        public string Name { get; set; }
+        [ExcelColumn(2, "姓名")]
+        public string Age { get; set; }
+        [ExcelColumn(4, "姓名")]
+        public string Sex { get; set; }
+        [ExcelColumn(3, "姓名")]
+        public DateTime Brithday { get; set; }
+    }
+}
